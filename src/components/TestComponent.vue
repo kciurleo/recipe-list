d2657d93fa68df6a2b6b4b908bd352fc7a4f3ced
Katie Ciurleo <katieciurleo@gmail.com>
2021-08-03 15:50:09 -0700

    setup for sarah

---
+++ src/components/TestComponent.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>hi sarah this is where you html</h1>
+    <p>everything has to go into one big div in the template every 
+    time but after that you can do div section etc however ya want</p>
+    <p>this is a test component within the homepage to fiddle around with</p>
+    <p>components are things that you put within a page basically, 
+    so you could have a whole bunch of different things on a page like a
+    search form and like a thing of response stuff and like a grocery list
+    section idk that's just my thoughts maybe we want more components or 
+    maybe that's enough? idk it's a way to separate things that you can 
+    then all import into one thing</p>
+    <p>they're good for organization or like style stuff because you can style 
+    components and main view pages differently; see the style section for
+    where to put css</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TestComponent',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,58 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TestComponent />
+    <h2>this is also where you html </h2>
+    <p>this is a view though which is suppoed to be like a static page like
+    a home or an about or something, which you put the compononents (sections,
+    whatever) onto in some sort of layout (it's actually probably bad practice
+    that i'm writing so much here but whatever)</p>
+    <p>(there's also the app page that's kinda where everything gets put into
+    but that doesn't really matter unless we wanna change the navigation etc)</p>
+    <p>every time you want to import a component you just do the three things
+    this one has; you import it in the script, list it under components where
+    this page gets exported, and then put it like in the actual html wherever
+    you want it to go. pretty handy for organization</p>
+
+    <b-button @click="showScriptInfo">Click Me For Script Info</b-button>
+
+    <div v-if="showScript">
+      <p>Javascript stuff goes in the script section; you put any variables you
+      really wanna keep in the data return section, and define all your functions
+      in the methods section</p>
+      <p>then you can call functions at certain things the user does (like @click
+      or @change) or at certain times, which is what the 'created' section is
+      (there are other times you can call stuff too, like when the page is updated
+      after a form is submited and stuff like that) </p>
+      <p>anyway there's probably more stuff that's relevant to know but that's all
+      i can think of for now, have fun goofing off</p>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import TestComponent from '@/components/TestComponent.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
-  }
-}
+    TestComponent
+  },
+  data() { 
+    return {
+      testvariable: 'hi sarah',
+      showScript: false,
+    };
+  },
+  created: function() {
+    console.log(this.testvariable);
+  },
+  methods: {
+    testFunction() {
+      console.log(this.testvariable);
+    },
+    showScriptInfo() {
+      this.showScript = true;
+    },
+  },
+};
 </script>
