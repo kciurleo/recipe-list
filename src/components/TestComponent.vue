783b9b51d14fa2d213f4cc2de4796e58a5ff95e6
Katie Ciurleo <katieciurleo@gmail.com>
2021-08-04 08:40:28 -0700

    add router links

--- src/components/TestComponent.vue
+++ src/components/TestComponent.vue
@@ -3,7 +3,7 @@
     <h1>hi sarah this is where you html</h1>
     <p>everything has to go into one big div with class="content" in the template every 
     time but after that you can do div section etc however ya want</p>
-    <p>this is a test component within the homepage to fiddle around with</p>
+    <p>this is a <router-link to="/test">test component</router-link> within the homepage to fiddle around with</p>
     <p>components are things that you put within a page basically, 
     so you could have a whole bunch of different things on a page like a
     search form and like a thing of response stuff and like a grocery list
@@ -13,6 +13,10 @@
     <p>they're good for organization or like style stuff because you can style 
     components and main view pages differently; see the style section for
     where to put css</p>
+    <p>I added links to all the different components that you can see in router -> index.js;
+    you can go to one of those if you Just want to see that particular component instead of
+    how they all come together on the homepage. (those links would probably get deleted but 
+    I like doing that because then I can fool around on just one section of a page until I like it) </p>
   </div>
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import TestComponent from '../components/TestComponent.vue'
+import AddRecipe from '../components/AddRecipeForm.vue'
+import GroceryList from '../components/GroceryList.vue'
+import SearchRecipe from '../components/SearchRecipesForm.vue'
+import Results from '../components/SearchResults.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +14,31 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/test',
+    name: 'TestComponent',
+    component: TestComponent
+  },
+  {
+    path: '/addrecipe',
+    name: 'AddRecipe',
+    component: AddRecipe
+  },
+  {
+    path: '/grocerylist',
+    name: 'GroceryList',
+    component: GroceryList
+  },
+  {
+    path: '/searchrecipe',
+    name: 'SearchRecipe',
+    component: SearchRecipe
+  },
+  {
+    path: '/results',
+    name: 'Results',
+    component: Results
   }
 ]
 
