b57b3c48db9874dd1e397c0d269688b29e8c02b2
Katie Ciurleo <kciurleo@smith.edu>
2021-08-11 19:44:19 -0700

    start of form no ingredients

--- src/components/AddRecipeForm.vue
+++ src/components/AddRecipeForm.vue
@@ -5,6 +5,37 @@
     <!-- my thought was that this could be a form used to add a recipe like 
     where you put in the ingredients and stuff (or the link which would be
     cool but i have no clue if that's possible) -->
+  <form id="targform" @submit.prevent>
+    <b-field label="Recipe Name">
+      <b-input type="text" id="recipename" v-model="recipeinfo.recipename" required />
+    </b-field>
+    <b-field label="Submitter">
+      <b-input type="text" id="submitter" v-model="recipeinfo.submitter" required />
+    </b-field>
+    <b-checkbox v-model="recipeinfo.vegetarian" native-value="Vegetarian">
+      Vegetarian
+    </b-checkbox>
+    <b-checkbox v-model="recipeinfo.vegan" native-value="Vegan">
+      Vegan
+    </b-checkbox>
+    <b-field label="Total Time (in minutes)">
+      <b-numberinput v-model="recipeinfo.recipetime" :controls="false"></b-numberinput>
+    </b-field>
+    <b-field label="Link to Recipe">
+      <b-input type="text" id="recipelink" v-model="recipeinfo.recipelink" />
+    </b-field>
+    <b-field label="Directions (optional)">
+      <b-input type="textarea" v-model="recipeinfo.directions"></b-input>
+    </b-field>
+    <!-- button here that says add ingredient--> 
+    <!-- when button pressed, three new fields: ingredient name (typable select) and quantity (number) and type (select) -->
+    <!-- if we have ingredient already, the quantity and type selector appear -->
+    <!-- if not person has to choose -->
+    <b-field class="buttons">
+      <b-button @click="submitForm" type="submit">Submit</b-button>
+    </b-field>
+  </form>
+  <pre> {{ recipeinfo }} </pre>
   </div>
 </template>
 
@@ -15,9 +46,22 @@ export default {
   },
   data() { 
     return {
+      recipeinfo: {
+        recipename: '',
+        submitter: '',
+        vegetarian: false,
+        vegan: false,
+        recipetime: '',
+        recipelink: '',
+        directions:'',
+        ingredients: {},
+      },
     };
   },
   methods: {
+    submitForm(){
+      console.log(this.recipeinfo)
+    },
   },
 };
 </script>
